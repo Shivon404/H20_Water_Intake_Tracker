32721467eea663b34202cf7a617c065d3195abee
Shivon <23101451@usc.edu.ph>
2025-04-19 00:19:57 +0800

    feat: added sign up UI layout

---
+++ app/src/main/java/com/shysoftware/h20tracker/model/User.java
@@ -0,0 +1,145 @@
+package com.shysoftware.h20tracker.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+public class User {
+
+    private String userId;
+    private String username;
+    private Double locationLat;
+    private Double locationLong;
+    private String address;
+    private LocalDate dateOfBirth;
+    private Double height;
+    private Double weight;
+    private Gender gender;
+    private Integer streak;
+    private Double xp;
+    private Rank rank;
+    @SerializedName("updatedAt")
+    private ZonedDateTime updatedAt;
+
+    public User(String userId, String username, Double locationLat, Double locationLong, String address, LocalDate dateOfBirth, Double height, Double weight, Gender gender, Integer streak, Double xp, Rank rank, ZonedDateTime updatedAt) {
+        this.userId = userId;
+        this.username = username;
+        this.locationLat = locationLat;
+        this.locationLong = locationLong;
+        this.address = address;
+        this.dateOfBirth = dateOfBirth;
+        this.height = height;
+        this.weight = weight;
+        this.gender = gender;
+        this.streak = streak;
+        this.xp = xp;
+        this.rank = rank;
+        this.updatedAt = updatedAt;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Double getLocationLat() {
+        return locationLat;
+    }
+
+    public void setLocationLat(Double locationLat) {
+        this.locationLat = locationLat;
+    }
+
+    public Double getLocationLong() {
+        return locationLong;
+    }
+
+    public void setLocationLong(Double locationLong) {
+        this.locationLong = locationLong;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Integer getStreak() {
+        return streak;
+    }
+
+    public void setStreak(Integer streak) {
+        this.streak = streak;
+    }
+
+    public Double getXp() {
+        return xp;
+    }
+
+    public void setXp(Double xp) {
+        this.xp = xp;
+    }
+
+    public Rank getRank() {
+        return rank;
+    }
+
+    public void setRank(Rank rank) {
+        this.rank = rank;
+    }
+
+    public ZonedDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(ZonedDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
---
+++ app/src/main/java/com/shysoftware/h20tracker/views/SignUpActivity.java
@@ -0,0 +1,23 @@
+package com.shysoftware.h20tracker.views;
+
+import static android.os.Build.VERSION_CODES.R;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.shysoftware.h20tracker.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+    }
+}
